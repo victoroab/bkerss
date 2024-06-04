09b78edf1846613670aec7045d6f2227663c00c7
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-04 11:17:29 +0100

    core(requirements): Wrote project overview

---
+++ requirements.md
@@ -0,0 +1,5 @@
+## OVERVIEW
+
+A platform for bakers to sign up and create profiles,
+upload pictures of their products with tags, create and join groups with ability to send messages and images
+search for other bakers and groups and posts with tags and follow other bakers
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import { getCountries, createCountry } from './functions'
-export default async function Home() {
-  const countries = await getCountries()
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      List of Countries:
-      <h1 className="font-semibold text-2xl">
-        {countries.length < 1 && 'no countries found'}
-      </h1>
-      {countries.map((country) => (
-        <h1 key={country.id}>{country.name}</h1>
-      ))}
-      <form action={createCountry} className="rounded-md space-x-4 mt-3">
-        <input type="text" name="name" className="border rounded-md p-2" />
-        <button type="submit" className="p-2 bg-sky-700 text-white rounded-md">
-          Create Country
-        </button>
-      </form>
-    </main>
+    <main className="border-flex min-h-screen flex-col items-center p-24"></main>
   )
 }
