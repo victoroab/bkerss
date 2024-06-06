5eadae8f6f39b8a446133bc78bba79f73a2324a4
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-06 20:22:45 +0100

    ui(landing page): made header border show on scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,13 @@ export default function Home() {
     <div className="w-full">
       <Header />
       <main className="border-flex min-h-screen flex-col items-center pt-36">
-        <div className="h-[90rem] container flex flex-col items-center">
-          <span className="text-6xl font-bold text-balance md:max-w-4xl text-center leading-tight">
-            The Best Platform For <span className="text-sky-900">Bakers</span>{' '}
-            All Around The World
+        <div className="container h-[90rem] flex flex-col items-center">
+          <span className="text-6xl font-bold text-balance md:max-w-4xl text-center leading-tight tracking-tight">
+            The Best <span className="text-sky-900">Social</span> Platform For{' '}
+            <span className="text-sky-900">Bakers</span> All Around The World
           </span>
         </div>
       </main>
     </div>
-    // <div className="border">
-    // </div>
   )
 }
--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -1,15 +1,35 @@
+'use client'
+
 import { Button } from './ui/button'
 import { Pacifico } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import { useState } from 'react'
 
 const pacifico = Pacifico({
   weight: '400',
   subsets: ['latin'],
 })
 
+const LOWER = 20
+const UPPER = 784
+
 export function Header() {
+  const [isScroll, setIsScroll] = useState<0 | 1>(0)
+  window.addEventListener('scroll', () => {
+    if (window.scrollY < LOWER) {
+      setIsScroll(0)
+    }
+    if (window.scrollY > LOWER && window.scrollY < UPPER) {
+      setIsScroll(1)
+    }
+  })
+
   return (
-    <header className="w-full z-20 fixed border flex items-center backdrop-blur-md">
+    <header
+      className={`w-full inset-x-0 transition-all duration-150 sticky top-0 z-20 flex items-center backdrop-blur-md ${
+        isScroll > 0 && 'border-b'
+      }`}
+    >
       <nav className="container flex items-center justify-between py-3">
         <span
           className={cn(pacifico.className, 'text-2xl font-bold text-sky-900')}
