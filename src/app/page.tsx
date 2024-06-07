de7630a6a0899befb26c6e7028389fb134163fab
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-07 15:15:38 +0100

    ui: set font for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import { Header } from '@/components/headers'
+import { cn } from '@/lib/utils'
+import { Mitr } from 'next/font/google'
+
+const montseMitr = Mitr({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function Home() {
   return (
@@ -6,7 +13,12 @@ export default function Home() {
       <Header />
       <main className="border-flex min-h-screen flex-col items-center pt-36">
         <div className="container h-[90rem] flex flex-col items-center">
-          <span className="text-6xl font-bold text-balance md:max-w-4xl text-center leading-tight tracking-tight">
+          <span
+            className={cn(
+              montseMitr.className,
+              'text-6xl font-bold text-balance md:max-w-4xl text-center leading-tight tracking-tight'
+            )}
+          >
             The Best <span className="text-sky-900">Social</span> Platform For{' '}
             <span className="text-sky-900">Bakers</span> All Around The World
           </span>
