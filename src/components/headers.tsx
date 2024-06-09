3614d72e93e4d1153ee8692892531d70a0a12c22
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-09 19:23:18 +0100

    ui: sign in button redirect and mount error fix

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -3,17 +3,11 @@
 import { Button } from './ui/button'
 import { Pacifico } from 'next/font/google'
 import { cn } from '@/lib/utils'
-import { useState } from 'react'
-import { SignInButton, SignedIn, SignIn } from '@clerk/nextjs'
+import { useEffect, useState } from 'react'
+import { SignedIn, SignIn } from '@clerk/nextjs'
 import { SignOutButton, SignedOut } from '@clerk/clerk-react'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTrigger,
-} from './ui/dialog'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import Link from 'next/link'
 
 const pacifico = Pacifico({
   weight: '400',
@@ -23,16 +17,22 @@ const pacifico = Pacifico({
 const LOWER = 20
 const UPPER = 784
 
+const REDIRECT_URL = process.env
+  .NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL as string
+
 export function Header() {
   const [isScroll, setIsScroll] = useState<0 | 1>(0)
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < LOWER) {
-      setIsScroll(0)
-    }
-    if (window.scrollY > LOWER && window.scrollY < UPPER) {
-      setIsScroll(1)
-    }
-  })
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY < LOWER) {
+        setIsScroll(0)
+      }
+      if (window.scrollY > LOWER && window.scrollY < UPPER) {
+        setIsScroll(1)
+      }
+    })
+  }, [])
 
   return (
     <header
@@ -41,11 +41,16 @@ export function Header() {
       }`}
     >
       <nav className="container flex items-center justify-between py-3">
-        <span
-          className={cn(pacifico.className, 'text-3xl font-bold text-sky-900')}
-        >
-          Bkerss
-        </span>
+        <Link href={'/'}>
+          <span
+            className={cn(
+              pacifico.className,
+              'text-3xl font-bold text-sky-900'
+            )}
+          >
+            Bkerss
+          </span>
+        </Link>
         <div className="flex items-center gap-4">
           <span className="cursor-pointer font-medium">
             <SignedIn>
@@ -56,6 +61,8 @@ export function Header() {
                 <DialogTrigger>Login</DialogTrigger>
                 <DialogContent className="w-fit p-0 pt-9 bg-white">
                   <SignIn
+                    routing="hash"
+                    forceRedirectUrl={REDIRECT_URL}
                     appearance={{
                       elements: {
                         cardBox: 'border-none shadow-none',
@@ -68,9 +75,11 @@ export function Header() {
               </Dialog>
             </SignedOut>
           </span>
-          <Button className="rounded-3xl bg-sky-500 hover:bg-sky-600">
-            Get Started
-          </Button>
+          <SignedOut>
+            <Button className="rounded-3xl bg-sky-500 hover:bg-sky-600" asChild>
+              <Link href={'/sign-up'}>Get Started</Link>
+            </Button>
+          </SignedOut>
         </div>
       </nav>
     </header>
@@ -106,12 +115,7 @@ export function AppHeader() {
             <li className="cursor-pointer hover:text-sky-600">Groups</li>
           </ul>
         </menu>
-        <div className="flex items-center gap-4">
-          {/* <span className="cursor-pointer font-medium">Login</span>
-          <Button className="rounded-3xl bg-sky-700 hover:bg-sky-600">
-            Get Started
-          </Button> */}
-        </div>
+        <div className="flex items-center gap-4"></div>
       </nav>
     </header>
   )
