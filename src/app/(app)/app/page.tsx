3b94a643038ac3c92edef8d0b3b6921d11b51397
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-12 04:24:10 +0100

    ui: profile card

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -1,8 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { User } from 'lucide-react'
+
 export default function Page() {
   return (
     <div className="w-full flex flex-col flex-1">
       <h1 className="text-3xl font-bold">Home</h1>
-      <div className="my-auto"></div>
+      <div className="mt-7 py-2">
+        <div className="w-full flex flex-1 items-center border-green-200 h-full">
+          <Card className="cursor-pointer hover:shadow-md transition-shadow">
+            <CardHeader>
+              <CardTitle className="flex items-center justify-between gap-3">
+                Profile
+                <User />
+              </CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter className="text-sm">
+              View and make changes to your profile
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/theme-provider'
+import NextTopLoader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <NextTopLoader />
             {children}
           </ThemeProvider>
         </body>
