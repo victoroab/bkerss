a077b628183fb59be88118f0a3b31e0bea430cb7
victoroab <96608980+victoroab@users.noreply.github.com>
2024-06-13 11:29:48 +0100

    ui: dark mode layout

--- src/app/(profile)/layout.tsx
+++ src/app/(profile)/layout.tsx
@@ -11,26 +11,26 @@ export default function ProfileLayout({
     <main className="w-full min-h-screen">
       <AppHeader />
       <div className="w-full pt-14">
-        <aside className="min-h-screen fixed flex-items-center w-48 p-4 border-r bg-neutral-100">
+        <aside className="min-h-screen fixed flex-items-center w-48 p-4 border-r bg-neutral-100 dark:bg-neutral-950">
           <nav className="h-full flex flex-col items-start">
             <div className="w-full flex flex-col justify-start gap-3">
-              <span className="w-full pl-2 uppercase text-sm font-semibold text-sky-900">
+              <span className="w-full pl-2 uppercase text-sm font-semibold text-sky-900 dark:text-sky-600">
                 Pages
               </span>
               <Button
-                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
+                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg dark:text-white dark:hover:bg-neutral-800 dark:hover:border-none hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
                 size={'sm'}
               >
                 <LayoutDashboard /> Home
               </Button>
               <Button
-                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
+                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg dark:text-white dark:hover:bg-neutral-800 dark:hover:border-none hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
                 size={'sm'}
               >
                 <Newspaper /> Activity
               </Button>
               <Button
-                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
+                className="flex justify-start gap-3 py-0 pl-2 w-full rounded-lg dark:text-white dark:hover:bg-neutral-800 dark:hover:border-none hover:text-sky-700 hover:border hover:border-sky-200 hover:bg-sky-50 text-gray-900 bg-transparent"
                 size={'sm'}
               >
                 <User /> Groups
--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -2,8 +2,8 @@ export default function ProfilePage() {
   return (
     <div className="w-full min-h-80 h-96 flex flex-col flex-1">
       <h1 className="text-xl font-semibold">Profile</h1>
-      <div className="mt-4 py-3 border rounded-md bg-neutral-50 h-full">
-        <div className="w-full flex flex-1 items-center border-green-200 h-full"></div>
+      <div className="mt-4 py-3 border rounded-md bg-neutral-50 dark:bg-neutral-950 h-full">
+        <div className="w-full flex flex-1 items-center h-full"></div>
       </div>
     </div>
   )
